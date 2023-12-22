c0d6c483c2bdea25617ede90c5420aff635a7486
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2023-12-22 15:02:55 +0630

    [Complete] Rest Api Blog Ado Dot Net Read

---
+++ AKLMPSTYZDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -0,0 +1,49 @@
+﻿using AKLMPSTYZDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AKLMPSTYZDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoDotNetController : ControllerBase
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "AKLMPSTYZDotNetCore",
+            UserID = "sa",
+            Password = "sa@123"
+        };
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+
+            string query = @"SELECT [Blog_Id]
+                            ,[Blog_Title]
+                            ,[Blog_Author]
+                            ,[Blog_Content]
+                            FROM [dbo].[Tbl_Blog]";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            sqlDataAdapter.Fill(dt);
+
+            connection.Close();
+
+            List<BlogDataModel> lst = dt.AsEnumerable().Select(dr => new BlogDataModel
+            {
+                Blog_Id = Convert.ToInt32(dr["Blog_Id"]),
+                Blog_Title = Convert.ToString(dr["Blog_Title"]),
+                Blog_Author = Convert.ToString(dr["Blog_Author"]),
+                Blog_Content = Convert.ToString(dr["Blog_Content"]),
+            }).ToList();
+            return Ok(lst);
+        }
+    }
+}
