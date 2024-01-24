28f5edfae2dd481b9257bfe615a90629e49b8df9
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2024-01-24 22:03:06 +0630

    [Added] Asp.Net Core MVC

---
+++ AKLMPSTYZDotNetCore.MvcApp/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AKLMPSTYZDotNetCore.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKLMPSTYZDotNetCore.MvcApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
---
+++ AKLMPSTYZDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using AKLMPSTYZDotNetCore.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKLMPSTYZDotNetCore.MvcApp.Controllers
+{
+    // https://localhost:3000/blog/index
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BlogController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [ActionName("Index")]
+        public async Task<IActionResult> BlogIndex()
+        {
+            // "select * from tbl_blog with (nolock)"
+            List<BlogDataModel> lst = await _appDbContext.Blogs.AsNoTracking().ToListAsync();
+            //return View();
+            return View("BlogIndex", lst);
+        }
+    }
+}
