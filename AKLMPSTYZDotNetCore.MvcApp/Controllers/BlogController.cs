ed22dab265b9aa4c712fab0b2e672653edc6be23
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-02-05 21:44:44 +0630

    [Complete] Mvc Blog Create, Delete, Edit

--- AKLMPSTYZDotNetCore.MvcApp/Controllers/BlogController.cs
+++ AKLMPSTYZDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AKLMPSTYZDotNetCore.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace AKLMPSTYZDotNetCore.MvcApp.Controllers
@@ -14,8 +15,58 @@ namespace AKLMPSTYZDotNetCore.MvcApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var lst = await _appDbContext.Blogs.ToListAsync();
+            var lst = await _appDbContext.Blogs
+                .OrderByDescending(x => x.Blog_Id)
+                .ToListAsync();
             return View(lst);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(BlogDataModel requestModel)
+        {
+            await _appDbContext.Blogs.AddAsync(requestModel);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _appDbContext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+            if (item is null)
+                return RedirectToAction("Index");
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, BlogDataModel requestModel)
+        {
+            var item = await _appDbContext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+            if (item is null)
+                return RedirectToAction("Index");
+
+            item.Blog_Title = requestModel.Blog_Title;
+            item.Blog_Author = requestModel.Blog_Author;
+            item.Blog_Content = requestModel.Blog_Content;
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _appDbContext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+            if(item is null)
+                return RedirectToAction("Index");
+
+            _appDbContext.Remove(item);
+            await _appDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
