882a03001f25dfb64d6d66ed8f69f1da672df6b0
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-02-05 21:30:37 +0630

    [Complete] Mvc Blog Read

---
+++ AKLMPSTYZDotNetCore.MvcApp/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AKLMPSTYZDotNetCore.MvcApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKLMPSTYZDotNetCore.MvcApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
---
+++ AKLMPSTYZDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AKLMPSTYZDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BlogController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lst = await _appDbContext.Blogs.ToListAsync();
+            return View(lst);
+        }
+    }
+}
