7d766da660ea9f87ccb2bc83c35b69428f8f782d
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-02-20 21:52:07 +0630

    [Complete] Minimal API

---
+++ DotNetTrainingBatch2.MinimalApi/Features/Blog/BlogService.cs
@@ -0,0 +1,51 @@
+﻿using DotNetTrainingBatch2.MinimalApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetTrainingBatch2.MinimalApi.Features.Blog
+{
+    public static class BlogService
+    {
+        public static IEndpointRouteBuilder UseBlogService(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/blog/{pageNo}/{pageSize}", async ([FromServicesAttribute] AppDbContext dbContext, int pageNo, int pageSize) =>
+                {
+                    return await dbContext.Blogs
+                        .AsNoTracking()
+                        .OrderByDescending(x => x.Blog_Id)
+                        .Skip((pageNo - 1) * pageSize)
+                        .Take(pageSize)
+                        .ToListAsync();
+                })
+                .WithName("GetBlogs")
+                .WithOpenApi();
+
+            app.MapGet("/api/blog/{id}", async ([FromServicesAttribute] AppDbContext dbContext, int id) =>
+                {
+                    var item = await dbContext.Blogs
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(x => x.Blog_Id == id);
+                    if (item is null)
+                    {
+                        return Results.NotFound("No data found.");
+                    }
+
+                    return Results.Ok(item);
+                })
+                .WithName("GetBlog")
+                .WithOpenApi();
+
+            app.MapPost("/api/blog", async ([FromServicesAttribute] AppDbContext dbContext, [FromBody] BlogDataModel blog) =>
+                {
+                    await dbContext.Blogs.AddAsync(blog);
+                    var result = await dbContext.SaveChangesAsync();
+                    var message = result > 0 ? "Saving Successful." : "Saving Failed";
+                    return Results.Ok(message);
+                })
+                .WithName("CreateBlog")
+                .WithOpenApi();
+
+            return app;
+        }
+    }
+}
--- DotNetTrainingBatch2.MinimalApi/Program.cs
+++ DotNetTrainingBatch2.MinimalApi/Program.cs
@@ -1,9 +1,19 @@
+using DotNetTrainingBatch2.MinimalApi;
+using DotNetTrainingBatch2.MinimalApi.Features.Blog;
+using DotNetTrainingBatch2.MinimalApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
 var app = builder.Build();
 
@@ -15,30 +25,43 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseBlogService();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//app.MapGet("/api/blog/{pageNo}/{pageSize}", async ([FromServicesAttribute] AppDbContext dbContext, int pageNo, int pageSize) =>
+//    {
+//        return await dbContext.Blogs
+//            .AsNoTracking()
+//            .OrderByDescending(x => x.Blog_Id)
+//            .Skip((pageNo - 1) * pageSize)
+//            .Take(pageSize)
+//            .ToListAsync();
+//    })
+//    .WithName("GetBlogs")
+//    .WithOpenApi();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+//app.MapGet("/api/blog/{id}", async ([FromServicesAttribute] AppDbContext dbContext, int id) =>
+//    {
+//        var item = await dbContext.Blogs
+//            .AsNoTracking()
+//            .FirstOrDefaultAsync(x => x.Blog_Id == id);
+//        if (item is null)
+//        {
+//            return Results.NotFound("No data found.");
+//        }
+
+//        return Results.Ok(item);
+//    })
+//    .WithName("GetBlog")
+//    .WithOpenApi();
+
+//app.MapGet("/api/blog", async ([FromServicesAttribute] AppDbContext dbContext, BlogDataModel blog) =>
+//    {
+//        await dbContext.Blogs.AddAsync(blog);
+//        var result = await dbContext.SaveChangesAsync();
+//        var message = result > 0 ? "Saving Successful." : "Saving Failed";
+//        return Results.Ok(message);
+//    })
+//    .WithName("CreateBlog")
+//    .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
