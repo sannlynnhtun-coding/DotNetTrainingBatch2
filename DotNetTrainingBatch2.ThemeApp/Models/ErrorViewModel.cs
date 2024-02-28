63cfbfafedce4aadba96062629bfbf6932bedbc6
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-02-28 21:49:51 +0630

    [Complete] Theme App

---
+++ DotNetTrainingBatch2.ThemeApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetTrainingBatch2.ThemeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
